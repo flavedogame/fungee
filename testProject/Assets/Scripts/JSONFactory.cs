7b20e54d5f1995385859492feabd20ff89b3285e
shshwdr <cs.vivian04@gmail.com>
2017-12-12 14:56:56 -0800

    first commit

---
+++ testProject/Assets/Scripts/JSONFactory.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using LitJson;
+
+//list of JSON file with path extensions
+//take in scene number, output Narrative Event
+//only NarrativeManager should be able to use this script
+
+namespace JSONFactory{
+	class JSONAssembly{
+		private static Dictionary<int,  string> _resourceList = new Dictionary<int,string> {
+			{1,"/Resources/Event1.json"}
+		};
+
+		private static string PathForScene(int sceneNumber){
+			string resourcePathResult;
+			if (_resourceList.TryGetValue (sceneNumber, out resourcePathResult)) {
+				return _resourceList [sceneNumber];
+			} else {
+				throw new Exception ("the scene number you provided is not inthe resource list");
+			}
+		}
+		private static bool IsValidJSON(string path) {
+			return (Path.GetExtension (path) == ".json") ? true : false;
+		}
+		public static NarrativeEvent RunJSONFactoryForScene(int sceneNumber) {
+			string resourcePath = PathForScene (sceneNumber);
+			if (IsValidJSON (resourcePath)) {
+				string jsonString = File.ReadAllText (Application.dataPath + resourcePath);
+				NarrativeEvent narrativeEvent = JsonMapper.ToObject<NarrativeEvent> (jsonString);
+				return narrativeEvent;
+			} else {
+				throw new Exception ("the json is not valid");
+			}
+		}
+	}
+}
---
+++ testProject/Assets/Scripts/NarrativeEvent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NarrativeEvent{
+	public List<Dialogue> dialogues;
+
+}
+
+public enum CharacterType{
+	Hero,Ally, Mentor
+}
+
+public struct Dialogue{
+	public CharacterType characterType;
+	public string name;
+	public string atlasImageName;
+	public string dialogueText;
+}
